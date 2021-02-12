c4054d91f43185005c9b2659cd46c00a1323dd0f
Afonso Costa <afonsir17@gmail.com>
2021-02-12 09:14:46 -0300

    Testing Calculator#sum with negative and positive numers

--- rspec_example/spec/calculator/calculator_spec.rb
+++ rspec_example/spec/calculator/calculator_spec.rb
@@ -3,17 +3,19 @@
 require 'calculator'
 
 describe Calculator do
-  it 'use sum method for 2 numbers' do
-    calc   = Calculator.new
-    result = calc.sum(5, 7)
+  context '#sum' do
+    it 'use sum method for 2 numbers' do
+      calc   = Calculator.new
+      result = calc.sum(5, 7)
 
-    expect(result).to eq(12)
-  end
+      expect(result).to eq(12)
+    end
 
-  it 'use sum method for 2 numbers with negative numbers' do
-    calc   = Calculator.new
-    result = calc.sum(-5, 7)
+    it 'use sum method for 2 numbers with negative numbers' do
+      calc   = Calculator.new
+      result = calc.sum(-5, 7)
 
-    expect(result).to eq(2)
+      expect(result).to eq(2)
+    end
   end
 end
