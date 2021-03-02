c977c1a461fb841d7b48e9576b64421e720500d3
AfonSir <afonsir17@gmail.com>
2021-03-02 17:42:28 -0300

    Testing: dealing with errors

--- rspec_example/lib/calculator.rb
+++ rspec_example/lib/calculator.rb
@@ -4,4 +4,8 @@ class Calculator
   def sum(number, another_number)
     number + another_number
   end
+
+  def div(numerator, denominator)
+    numerator / denominator
+  end
 end
--- rspec_example/spec/calculator/calculator_spec.rb
+++ rspec_example/spec/calculator/calculator_spec.rb
@@ -3,6 +3,15 @@
 require 'calculator'
 
 describe Calculator do
+  context '#div' do
+    it 'divided by 0', :aggregate_failures do
+      expect { subject.div(3, 0) }.to raise_error(ZeroDivisionError)
+      expect { subject.div(3, 0) }.to raise_error('divided by 0')
+      expect { subject.div(3, 0) }.to raise_error(ZeroDivisionError, 'divided by 0')
+      expect { subject.div(3, 0) }.to raise_error(/divided/)
+    end
+  end
+
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5, 7)
